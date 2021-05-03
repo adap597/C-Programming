d20575706cc7c05711ff782aa396e037a61a2ecf
Aurora <82787709+adap597@users.noreply.github.com>
2021-05-03 13:57:39 -0700

    Update Midterm Assignment 3.cpp

--- Midterm Assignment 3.cpp
+++ Midterm Assignment 3.cpp
@@ -1,6 +1,20 @@
+/*Write a program that contains the function measure() that is to accept a long integer number total and the addresses
+of the integer variables inches, feet, yards, and miles. The passed long integer represents the total number of inches, 
+and the function is to determine the number of miles, yards, feet, and inches in the passed value, 
+writing these values directly into the respective variables declared in the calling function.
+
+This function will be called from the main program and when it returns to main, 
+it will print out the values of inches, feet, yards, and miles.
+
+Use the relationships:
+inch
+foot (12 inches)
+yard (36 inches; 3 feet)
+mile (5280 feet; 1760 yards; 63,360 inches)
+*/
+
 // Midterm Assignment 3.cpp : This program accepts a total number of inches input by the user, then calculates and displays the total number of feet, yards, and miles
-//Name: Aurora DiPeso
-//University ID: 0663597
+
 //Thomas Edison State University
 //C Programming: COS-116
 //Section no.: OL009
