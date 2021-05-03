786afbb3ab5c3f7e09107eedb5a82b9e52c54a39
Aurora <82787709+adap597@users.noreply.github.com>
2021-05-03 13:54:42 -0700

    Update Written Assignment 3B.cpp

--- Written Assignment 3B.cpp
+++ Written Assignment 3B.cpp
@@ -1,6 +1,25 @@
+/*Write a C function named liquid() that is to accept an integer number and the addresses of the variables gallons, quarts, pints, and cups. 
+The passed integer represents the total number of cups, and the function is to determine the number of gallons, quarts, pints, and cups in the passed value. 
+Using the passed addresses, the function should directly alter the respective variables in the calling function. 
+Use the relationships of 2 cups to a pint, 4 cups to a quart, and 16 cups to a gallon.
+Include this function in a program, printing the results after the function has run.
+
+If the user enters 27 cups, the output should be:
+
+1 gallon
+2 quarts
+1 pint
+1 cup
+
+and not:
+
+1 gallon
+6 quarts
+13 pints
+27 cups 
+*/
+
 // Written Assignment 3B.cpp : This program determines the amount of gallons, quarts, pints, and cups are contained in the value provided by the user (total cups)
-//Name: Aurora DiPeso
-//University ID: 0663597
 //Thomas Edison State University
 //C Programming: COS-116
 //Section No.: OL009
